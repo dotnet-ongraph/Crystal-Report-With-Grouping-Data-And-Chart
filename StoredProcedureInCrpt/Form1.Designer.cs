561cec25a221af6f4154017283ff8843ba044eda
AdityaSrivastava <asrivastava@reliablegroup.com>
2021-05-14 18:27:09 +0530

    Add project files.

---
+++ StoredProcedureInCrpt/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace StoredProcedureInCrpt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crvReport = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.CountryRegionRptbutton = new System.Windows.Forms.Button();
+            this.MonthlySalesRptbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // crvReport
+            // 
+            this.crvReport.ActiveViewIndex = -1;
+            this.crvReport.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crvReport.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crvReport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crvReport.Location = new System.Drawing.Point(0, 0);
+            this.crvReport.Name = "crvReport";
+            this.crvReport.Size = new System.Drawing.Size(1042, 430);
+            this.crvReport.TabIndex = 0;
+            this.crvReport.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            // 
+            // CountryRegionRptbutton
+            // 
+            this.CountryRegionRptbutton.Location = new System.Drawing.Point(686, 0);
+            this.CountryRegionRptbutton.Name = "CountryRegionRptbutton";
+            this.CountryRegionRptbutton.Size = new System.Drawing.Size(170, 23);
+            this.CountryRegionRptbutton.TabIndex = 2;
+            this.CountryRegionRptbutton.Text = "Country Region Report";
+            this.CountryRegionRptbutton.UseVisualStyleBackColor = true;
+            this.CountryRegionRptbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MonthlySalesRptbutton
+            // 
+            this.MonthlySalesRptbutton.Location = new System.Drawing.Point(490, 0);
+            this.MonthlySalesRptbutton.Name = "MonthlySalesRptbutton";
+            this.MonthlySalesRptbutton.Size = new System.Drawing.Size(163, 23);
+            this.MonthlySalesRptbutton.TabIndex = 3;
+            this.MonthlySalesRptbutton.Text = "Monthly Sales Order Report";
+            this.MonthlySalesRptbutton.UseVisualStyleBackColor = true;
+            this.MonthlySalesRptbutton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 430);
+            this.Controls.Add(this.MonthlySalesRptbutton);
+            this.Controls.Add(this.CountryRegionRptbutton);
+            this.Controls.Add(this.crvReport);
+            this.Name = "Form1";
+            this.Text = "Crystal Report";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crvReport;
+        private System.Windows.Forms.Button CountryRegionRptbutton;
+        private System.Windows.Forms.Button MonthlySalesRptbutton;
+    }
+}
+
---
+++ StoredProcedureInCrpt/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StoredProcedureInCrpt
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=HP-NETBOOK\SQLEXPRESS01;Initial Catalog=AdventureWorks2016;
+                                                    User ID=sa;Password=appforms@123;integrated Security=True;");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+            SqlDataAdapter sqlDa1 = new SqlDataAdapter("OrderRegionReport", sqlCon);
+            SqlDataAdapter sqlDa2 = new SqlDataAdapter("OrderWithDetails", sqlCon);
+            sqlDa1.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlDa2.SelectCommand.CommandType = CommandType.StoredProcedure;
+            //sqlDa.SelectCommand.Parameters.AddWithValue("@Price",0);
+            DataTable dtb1 = new DataTable();
+            sqlDa1.Fill(dtb1);
+            DataTable dtb2 = new DataTable();
+            sqlDa2.Fill(dtb2);
+            sqlCon.Close();
+            CrystalReport.crptCountryRegionOrders crpSalesOrder = new CrystalReport.crptCountryRegionOrders();
+            crpSalesOrder.Database.Tables["OrderRegionReport"].SetDataSource(dtb1);
+            crpSalesOrder.Database.Tables["OrderWithDetails"].SetDataSource(dtb2);
+            crvReport.ReportSource = null;
+            crvReport.ReportSource = crpSalesOrder;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+            SqlDataAdapter sqlDa1 = new SqlDataAdapter("AnnuallyOrderReport", sqlCon);
+            SqlDataAdapter sqlDa2 = new SqlDataAdapter("MonthlySalesOrderData", sqlCon);
+            sqlDa1.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlDa2.SelectCommand.CommandType = CommandType.StoredProcedure;
+            //sqlDa.SelectCommand.Parameters.AddWithValue("@Price",0);
+            DataTable dtb1 = new DataTable();
+            sqlDa1.Fill(dtb1);
+            DataTable dtb2 = new DataTable();
+            sqlDa2.Fill(dtb2);
+            sqlCon.Close();
+            CrystalReport.crptMonthlySalesOrder crpSalesOrder = new CrystalReport.crptMonthlySalesOrder();
+            crpSalesOrder.Database.Tables["AnnuallyOrderReport"].SetDataSource(dtb1);
+            crpSalesOrder.Database.Tables["MonthlySalesOrderData"].SetDataSource(dtb2);
+            crvReport.ReportSource = null;
+            crvReport.ReportSource = crpSalesOrder;
+        }
+    }
+}
